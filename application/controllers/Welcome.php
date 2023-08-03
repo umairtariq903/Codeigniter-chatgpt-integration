84626d64015a15348b7c6c8c9e1949bac013ddef
Umair Tariq <umairtariq903@yahoo.com>
2023-08-03 06:06:57 -0700

    Format welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,26 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined("BASEPATH") or exit("No direct script access allowed");
 
-class Welcome extends CI_Controller {
-
-
-	public function __construct()
+class Welcome extends CI_Controller
+{
+    public function __construct()
     {
         parent::__construct();
-        $this->load->model('chatgpt');
-        $this->load->helper('form');
+        $this->load->model("chatgpt");
+        $this->load->helper("form");
     }
 
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function index()
+    {
+        $this->load->view("welcome_message");
+    }
 
-	public function chat()
-	{
-		$userQuery = $this->input->post('user_query');
-		$botReply = $this->chatgpt->chatApi($userQuery);
-		$data['botReply'] = $botReply;
-		$this->load->view('bot_reply', $data);	
-	}
+    public function chat()
+    {
+        $userQuery = $this->input->post("user_query");
+        $botReply = $this->chatgpt->chatApi($userQuery);
+        $data["botReply"] = $botReply;
+        $this->load->view("bot_reply", $data);
+    }
 }
